6d26adff984c81baf1deec31decc13f7faa41f8e
akhil chella <akilchella2020@gmail.com>
2022-12-14 16:18:28 -0600

    array list demo

---
+++ ArrayListDemo.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+    public static void main(String[] args){
+        System.out.println("this is ArryaList Tutorial");
+        ArrayList<String> ar1 = new ArrayList<>();
+        ar1.add("akj");
+        ar1.add("sde");
+        ar1.add("che");
+
+        ArrayList<String> ar2 = new ArrayList<>();
+        ar2.add("bpu");
+        ar2.add("csalkc");
+        ar2.add("sfhd");
+        System.out.println(ar2);
+        System.out.println("------------");
+        System.out.println(ar1);
+
+    }
+}
