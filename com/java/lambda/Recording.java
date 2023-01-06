64e8a1d092d8010db9297430b7af4f93a1a5e5ff
achella2022 <akilchella2020@gmail.com>
2023-01-05 21:29:15 -0600

    Local and global variables

---
+++ com/java/lambda/Recording.java
@@ -0,0 +1,21 @@
+package com.java.lambda;
+
+public class Recording {
+    public String name;
+    private int age;
+    public Recording(String NameRec){
+        name = NameRec;
+    }
+    private void SetAge(int AgeRec){
+        age = AgeRec;
+    }
+    public void printRec(){
+        System.out.println("name:" + name);
+        System.out.println("age:" + age);
+    }
+    public static void main(String[] args) {
+        Recording r = new Recording("Krisna");
+        r.SetAge(23);
+        r.printRec();
+    }
+}
